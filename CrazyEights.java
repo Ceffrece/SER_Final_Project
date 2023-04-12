d61bcf66e2b8b2b29bb1dd9149f29cb0613e228e
Ceffrece <cheffrece@quinnipiac.edu>
2023-04-12 16:52:39 -0400

    gave player their hand

--- CrazyEights.java
+++ CrazyEights.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
 
 public class CrazyEights {
 	public static void main(String[] args) {
-		//initial commit
+		System.out.println("Welcome to Crazy Eights!!");
+		ArrayList<Card> playerHand = new ArrayList<Card>();
+		System.out.println("Game Start");
+		DeckOfCards deck = new DeckOfCards();
+		deck.shuffle();
+		for(int count = 0; count < 5; count++){
+			playerHand.add(deck.dealCard());
+		}
+		String displayHand = "";
+		for(Card item: playerHand){
+			displayHand += "\n" + item;
+		}
+		System.out.println(displayHand);
 	}
 }
