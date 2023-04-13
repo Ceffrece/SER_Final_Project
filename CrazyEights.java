f7c31275f1c141c31ec2f8a864aa8ecd5e5b09d8
Ceffrece <cheffrece@quinnipiac.edu>
2023-04-13 16:03:45 -0400

    combined previous changes

--- CrazyEights.java
+++ CrazyEights.java
@@ -23,13 +23,12 @@ public class CrazyEights {
 			player1Hand.add(deck.dealCard());
 			player2Hand.add(deck.dealCard());
 		}
-<<<<<<< HEAD
 		while(!(player1Hand.isEmpty()) || !(player2Hand.isEmpty())){
 			Card faceUp = deck.dealCard();
 			boolean playedCard = false;
 			for(Card card: player1Hand){
 				if(isCardPlayable(card,faceUp)){
-					//displayHand();
+					CrazyEights.displayHand(player1Hand);
 					playedCard = true;
 				}
 			}
@@ -37,7 +36,9 @@ public class CrazyEights {
 				
 			}
 		}
+	}
 
+	public static void displayHand(ArrayList<Card> playerHand){
 		//DisplayHand Function
 		String displayHand = "";
 		for(Card item: playerHand){
@@ -67,37 +68,5 @@ public class CrazyEights {
 		for(Card item: playerHand){
     		displayHand += "+-----+ ";
 		}
-			System.out.println(displayHand);
-=======
-		String displayHand = "";
-		for(Card item: playerHand){
-    		displayHand += "+-----+ ";
-		}
-		displayHand += "\n";
-		for(Card item: playerHand){
-    		displayHand += "|     | ";
-		}
-		displayHand += "\n";
-		for(Card item: playerHand){
-			String face = item.face;
-            if (face.length() == 1) {
-                face = " " + face; 
-            }
-            displayHand += "|  " + face + " | ";
-        }
-		displayHand += "\n";
-		for(Card item: playerHand){
-    		displayHand += "|  " + item.suit.charAt(0) + "  | ";
-		}
-		displayHand += "\n";
-		for(Card item: playerHand){
-    		displayHand += "|     | ";
-		}
-		displayHand += "\n";
-		for(Card item: playerHand){
-    		displayHand += "+-----+ ";
-		}
-			System.out.println(displayHand);
->>>>>>> 8b98df9a8b60a6ab575b659ced6b9980053329df
 	}
 }
