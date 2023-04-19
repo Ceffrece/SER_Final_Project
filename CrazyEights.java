b47660540bc33e52ad4ca5de3110b3199aa70bf4
Ceffrece <cheffrece@quinnipiac.edu>
2023-04-19 14:32:56 -0400

    changes

--- CrazyEights.java
+++ CrazyEights.java
@@ -5,7 +5,7 @@ public class CrazyEights {
 		if(faceUp.getFace().equals(card.getFace()) || faceUp.getSuit().equals(card.getSuit())){
 			return true;
 		}
-		if(card.getFace().equals("Eight")){
+		if(card.getFace().equals("8")){
 			return true;
 		}
 		else{
@@ -16,15 +16,20 @@ public class CrazyEights {
 		System.out.println("Welcome to Crazy Eights!!");
 		ArrayList<Card> player1Hand = new ArrayList<Card>();
 		ArrayList<Card> player2Hand = new ArrayList<Card>();
+
 		System.out.println("Game Start");
+
 		DeckOfCards deck = new DeckOfCards();
 		deck.shuffle();
+
 		for(int count = 0; count < 5; count++){
 			player1Hand.add(deck.dealCard());
 			player2Hand.add(deck.dealCard());
 		}
+
+		Card faceUp = deck.dealCard();
+
 		while(!(player1Hand.isEmpty()) || !(player2Hand.isEmpty())){
-			Card faceUp = deck.dealCard();
 			boolean playedCard = false;
 			for(Card card: player1Hand){
 				if(isCardPlayable(card,faceUp)){
@@ -33,6 +38,9 @@ public class CrazyEights {
 				}
 			}
 			if(playedCard){
+				break;
+			}
+			else{
 				
 			}
 		}
