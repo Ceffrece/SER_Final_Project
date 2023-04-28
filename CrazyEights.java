efc7860a4d0bcf19480d48c9949ea1d3a85804a1
Ceffrece <cheffrece@quinnipiac.edu>
2023-04-28 15:09:21 -0400

    fixed comment

--- CrazyEights.java
+++ CrazyEights.java
@@ -2,7 +2,6 @@
  * Charlize Aponte and Caden Effrece 
  * Date: 04/26/23
  * Description: This code runs and plays the game Crazy Eights in both single player and double player mode
- * sfiuygs 
  */
 import java.util.ArrayList;
 import java.util.Scanner;
